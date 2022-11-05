262121c81a69ea4a658a70e6ff98a5f9edf0b82a
Lucas <lucasrafael123araujolima@gmail.com>
2022-11-04 23:45:30 -0400

    feat: :zap: Mobile Init

---
+++ mobile/App.tsx
@@ -0,0 +1,15 @@
+import { StatusBar } from "expo-status-bar"
+import { NativeBaseProvider, Center, Text } from "native-base"
+
+import { THEME } from "./src/styles/theme"
+
+export default function App() {
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <Center flex={1} bgColor="black">
+        <Text>Hello React Native!</Text>
+        <StatusBar style="auto" />
+      </Center>
+    </NativeBaseProvider>
+  )
+}
