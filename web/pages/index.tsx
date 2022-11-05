eaf18fa48c4a85dd0e3b9383acde083c8ab4e6ce
Lucas <lucasrafael123araujolima@gmail.com>
2022-11-04 23:39:44 -0400

    feat: :sparkles: initial Mobile

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
-  return (
-    <h1>
-      <div>Hello World</div>
-    </h1>
-  )
+interface HomeProps {
+  count: number
+}
+
+export default function Home(props: HomeProps) {
+  return <h1>Contagem: {props.count}</h1>
+}
+
+export const getStaticProps = async () => {
+  const response = await fetch("http://localhost:3333/pools/count")
+  const data = await response.json()
+
+  console.log(data)
+
+  return {
+    props: {
+      count: data.count
+    }
+  }
 }
