9224d56d01fff686c60c8ba66f317deb0aa2d2ab
Lucas <lucasrafael123araujolima@gmail.com>
2022-11-06 22:39:57 -0400

    feat: :art: Axios

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -3,23 +3,30 @@ import appPreviewImg from "../assets/app-nlw-copa-preview.png"
 import logoImg from "../assets/logo.svg"
 import usersAvatarExampleImg from "../assets/users-avatar-example.png"
 import iconCheckImg from "../assets/icon-check.svg"
+import { api } from "../lib/axios"
 
-export default function Home() {
+interface HomeProps {
+  poolCount: number
+  guessCount: number
+  userCount: number
+}
+
+export default function Home(props: HomeProps) {
   return (
     <div className="max-w-[1124px] h-screen mx-auto grid grid-cols-2 gap-28 items-center">
       <main>
-        <Image src={logoImg} quality={100} alt="NLW Copa" />
+        <Image src={logoImg} alt="NLW Copa" />
 
         <h1 className="mt-14 text-white text-5xl font-bold leading-tight">
           Crie seu próprio bolão da copa e compartilhe entre amigos!
         </h1>
 
         <div className="mt-10 flex items-center gap-2">
-          <Image src={usersAvatarExampleImg} quality={100} alt="" />
+          <Image src={usersAvatarExampleImg} alt="" />
 
           <strong className="text-gray-100 text-xl">
-            <span className="text-ignite-500">+12.592</span> pessoas já estão
-            usando
+            <span className="text-ignite-500">+{props.userCount}</span> pessoas
+            já estão usando
           </strong>
         </div>
 
@@ -47,7 +54,7 @@ export default function Home() {
           <div className="flex items-center gap-6">
             <Image src={iconCheckImg} alt="" />
             <div className="flex flex-col">
-              <span className="font-bold text-2xl">+2.034</span>
+              <span className="font-bold text-2xl">+{props.poolCount}</span>
               <span>Bolões criados</span>
             </div>
           </div>
@@ -57,27 +64,35 @@ export default function Home() {
           <div className="flex items-center gap-6">
             <Image src={iconCheckImg} alt="" />
             <div className="flex flex-col">
-              <span className="font-bold text-2xl">+2.034</span>
+              <span className="font-bold text-2xl">+{props.guessCount}</span>
               <span>Palpites enviados</span>
             </div>
           </div>
         </div>
       </main>
 
-      <Image src={appPreviewImg} quality={100} alt="Dois Celulares" />
+      <Image
+        src={appPreviewImg}
+        alt="Dois celulares exibindo uma prévia da aplicação do NLW Copa"
+        quality={100}
+      />
     </div>
   )
 }
 
-// export const getStaticProps = async () => {
-//   const response = await fetch("http://localhost:3333/pools/count")
-//   const data = await response.json()
+export const getServerSideProps = async () => {
+  const [poolCountResponse, guessCountResponse, userCountResponse] =
+    await Promise.all([
+      api.get("pools/count"),
+      api.get("guesses/count"),
+      api.get("users/count")
+    ])
 
-//   console.log(data)
-
-//   return {
-//     props: {
-//       count: data.count
-//     }
-//   }
-// }
+  return {
+    props: {
+      poolCount: poolCountResponse.data.count,
+      guessCount: guessCountResponse.data.count,
+      userCount: userCountResponse.data.count
+    }
+  }
+}
