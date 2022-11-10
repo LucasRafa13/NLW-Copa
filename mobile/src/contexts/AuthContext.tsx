d1e4b65ad36813142ab4d8d26ed88078583f3137
Lucas <lucasrafael123araujolima@gmail.com>
2022-11-10 14:29:00 -0400

    feat: :fire: OAuth 2.0

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,4 +1,9 @@
-import { createContext } from "react"
+import { createContext, useState, useEffect } from "react"
+import * as Google from "expo-auth-session/providers/google"
+import * as AuthSession from "expo-auth-session"
+import * as WebBrowser from "expo-web-browser"
+
+WebBrowser.maybeCompleteAuthSession()
 
 interface UserProps {
   name: string
@@ -11,24 +16,53 @@ interface AuthProviderProps {
 
 export interface AuthContextDataProps {
   user: UserProps
+  isUserLoading: boolean
   signIn: () => Promise<void>
 }
 
 export const AuthContext = createContext({} as AuthContextDataProps)
 
 export function AuthContextProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<UserProps>({} as UserProps)
+  const [isUserLoading, setIsUserLoading] = useState(false)
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId:
+      "808971984770-t74j87tuia1ah60dmkhma5tuf9fdc6uj.apps.googleusercontent.com",
+    redirectUri: AuthSession.makeRedirectUri({
+      useProxy: true
+    }),
+    scopes: ["profile", "email"]
+  })
+
   async function signIn() {
-    console.log("Vamos logar")
+    try {
+      setIsUserLoading(true)
+      await promptAsync()
+    } catch (error) {
+      console.log(error)
+      throw error
+    } finally {
+      setIsUserLoading(false)
+    }
   }
+
+  async function signInWithGoogle(access_token: string) {
+    console.log("TOKEN ===>" + access_token)
+  }
+
+  useEffect(() => {
+    if (response?.type === "success" && response.authentication?.accessToken) {
+      signInWithGoogle(response.authentication.accessToken)
+    }
+  }, [response])
 
   return (
     <AuthContext.Provider
       value={{
         signIn,
-        user: {
-          name: "John Doe",
-          avatarUrl: "https://github.com/LucasRafa13.png"
-        }
+        isUserLoading,
+        user
       }}
     >
       {children}
